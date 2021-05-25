755b057f1d8f174557a296b5c8cccc1b133fca45
AdrianHDev <adrianhensondev@gmail.com>
2021-05-25 19:28:49 -0400

    Merge Fix

--- secret-recipes-front-end/src/App.js
+++ secret-recipes-front-end/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import AddRecipe from "./components/AddRecipe";
+import Homepage from './Components/Homepage/Homepage'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <AddRecipe />
+      <Homepage />
     </div>
   );
 }
