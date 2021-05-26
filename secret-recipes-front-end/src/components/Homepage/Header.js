2010c5f1703398c52da3f6d98ec9aa34cf26c6e5
AdrianHDev <adrianhensondev@gmail.com>
2021-05-26 13:05:37 -0400

    Update header

---
+++ secret-recipes-front-end/src/components/Homepage/Header.js
@@ -0,0 +1,16 @@
+
+
+
+const Header = (props) => {
+    const showModal = (ev) => {
+    }
+    
+    return (
+    <header className='navbar'>
+      <nav style={{ display: 'flex' }}>
+          <a href="/">Home</a>
+          <a href="/login">Login</a>
+      </nav>
+    </header>
+  )
+}
