f70c6be112f0da611411b12cb7ded1618434bbbd
AdrianHDev <adrianhensondev@gmail.com>
2021-05-25 21:16:15 -0400

    Clean up SignInForm

--- secret-recipes-front-end/src/components/Login/SignInForm.js
+++ secret-recipes-front-end/src/components/Login/SignInForm.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import {useState} from "react";
 import Button from "react-bootstrap/esm/Button";
 import Modal from "react-bootstrap/Modal";
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 
 const initialState = {
     userName: '',
-    password: '',
+    password: ''
 };
 
 
@@ -14,40 +14,40 @@ const SignInForm = (props) => {
 
     const changeHandler = (e) => {
         e.preventDefault();
-        setFormSign({ ...recipeData, [e.target.name]: e.target.value });
+        setFormSign({
+            ...recipeData,
+            [e.target.name]: e.target.value
+        });
     }
 
     return (
         <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.title>
-              Log-In
-            </Modal.title>
-          </Modal.Header>
-            <form>
-                <label htmlFor="userName">Username:</label>
+            <Modal.Header closeButton>
+                <Modal.title>
+                    Log-In
+                </Modal.title>
+            </Modal.Header>
+            <Modal.Body>
+                <form>
+                    <label htmlFor="userName">Username:</label>
 
-                <input
-                    name="userName"
-                    value={formSign.userName}
-                    onChange={changeHandler}
-                    className="form-control"
-                />
+                    <input name="userName"
+                        value={formSign.userName}
+                        onChange={changeHandler}
+                        className="form-control"/>
 
-                <label htmlFor="password">Password:</label>
+                    <label htmlFor="password">Password:</label>
 
-                <input
-                  name="password"
-                  tyep="password" 
-                  value={formSign.password}
-                  onChange={changeHandler}
-                  className="form-control"
-                />
-                <Modal.Footer>
-                  <Button onClick={redirectTo}>Log-In</Button>
-                  <Button onClick={navToLink}>Register</Button>
-                </Modal.Footer>
-              </form>
-          </Modal.Dialog>
+                    <input name="password" tyep="password"
+                        value={formSign.password}
+                        onChange={changeHandler}
+                        className="form-control"/>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={redirectTo}>Log-In</Button>
+                <Button onClick={navToLink}>Register</Button>
+            </Modal.Footer>
+        </Modal.Dialog>
     )
 }
