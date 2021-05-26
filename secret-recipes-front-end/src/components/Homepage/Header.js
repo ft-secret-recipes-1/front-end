2503c796dc6ae8b07c6a8a2e94b78e057ff99aef
AdrianHDev <adrianhensondev@gmail.com>
2021-05-26 13:25:49 -0400

    Basic navbar

--- secret-recipes-front-end/src/components/Homepage/Header.js
+++ secret-recipes-front-end/src/components/Homepage/Header.js
@@ -1,16 +1,17 @@
+import { Nav, Navbar, NavbarBrand } from 'react-bootstrap'
 
+const Header = props => {
+  const showModal = ev => {}
 
-
-const Header = (props) => {
-    const showModal = (ev) => {
-    }
-    
-    return (
-    <header className='navbar'>
-      <nav style={{ display: 'flex' }}>
-          <a href="/">Home</a>
-          <a href="/login">Login</a>
-      </nav>
-    </header>
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Brand href='/'>Secret Recipes!</NavbarBrand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse>
+        <Nav.Link href='/'>Home</Nav.Link>
+        <Nav.Link href='/signIn'>Sign-In</Nav.Link>
+        <Nav.Link href='/signUp'>Sign-Up</Nav.Link>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
