80ab8200b9619fec741be97bed003858217920bf
Kaseem Bradley <kaseembradley@Kaseems-MacBook-Air.local>
2021-05-27 09:52:04 -0400

    user can login and set token to local storage

--- secret-recipes-front-end/src/components/Login/SignInForm.js
+++ secret-recipes-front-end/src/components/Login/SignInForm.js
@@ -1,58 +1,80 @@
-import {useState} from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/esm/Button";
 import Modal from "react-bootstrap/Modal";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const initialState = {
-    userName: '',
-    password: ''
+  user_username: "Marge",
+  user_password: "A@bc.123",
 };
 
-
 const SignInForm = (props) => {
-    const [formSign, setFormSign] = useState(initialState);
+  const [formSign, setFormSign] = useState(initialState);
+  const history = useHistory();
 
-    const changeHandler = (e) => {
-        e.preventDefault();
-        setFormSign({
-            ...formSign,
-            [e.target.name]: e.target.value
-        });
-    }
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setFormSign({
+      ...formSign,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const navToLink = (ev) => {
-      console.log(ev.target.title)
-    }
+  const navToLink = (ev) => {
+    console.log(ev.target.title);
+  };
 
-    return (
-        <Modal.Dialog>
-            <Modal.Header closeButton>
-                <Modal.Title>
-                    Log-In
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <form>
-                    <label htmlFor="userName">Username:</label>
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formSign);
+    axios
+      .post(
+        "https://ft-bw-may-secret-family-recipe.herokuapp.com/api/auth/login",
+        formSign
+      )
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data);
+        history.push("/");
+      });
+  };
 
-                    <input name="userName"
-                        value={formSign.userName}
-                        onChange={changeHandler}
-                        className="form-control"/>
+  return (
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Log-In</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="userName">Username:</label>
 
-                    <label htmlFor="password">Password:</label>
+          <input
+            name="userName"
+            value={formSign.user_username}
+            onChange={changeHandler}
+            className="form-control"
+          />
 
-                    <input name="password" type="password"
-                        value={formSign.password}
-                        onChange={changeHandler}
-                        className="form-control"/>
-                </form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button title='' onClick={navToLink}>Log-In</Button>
-                <Button title='' onClick={navToLink}>Register instead</Button>
-            </Modal.Footer>
-        </Modal.Dialog>
-    )
-}
+          <label htmlFor="password">Password:</label>
+
+          <input
+            name="password"
+            type="password"
+            value={formSign.user_password}
+            onChange={changeHandler}
+            className="form-control"
+          />
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onSubmit}>Log-In</Button>
+        <Button title="" onClick={navToLink}>
+          Register instead
+        </Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  );
+};
 
 export default SignInForm;
